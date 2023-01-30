229c86321c4a852a42b77d02164bb56151d9e17a
Midknight-Maxielle <93679941+Midknight-Maxielle@users.noreply.github.com>
2023-01-30 12:31:48 -0500

    Ported to MC 1.19.2 / Forge 43.2+ Rewrote loot serializer system.

--- src/main/java/com/mk/archarsenal/ArchArsenal.java
+++ src/main/java/com/mk/archarsenal/ArchArsenal.java
@@ -2,10 +2,12 @@ package com.mk.archarsenal;
 
 import com.mk.archarsenal.entity.renderers.BarbedArrowRenderer;
 import com.mk.archarsenal.entity.renderers.VillagerArrowRenderer;
+import com.mk.archarsenal.loot.ModLootModifiers;
 import com.mk.archarsenal.util.KnightcoreBowModelProperties;
 import com.mk.archarsenal.util.registries.ModRegistry;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
@@ -17,10 +19,13 @@ public class ArchArsenal
     public static final String MOD_ID = "archarsenal";
 
     public ArchArsenal() {
+
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::bowSetup);
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::arrowSetup);
         ModRegistry.registerENTITY();
         ModRegistry.registerITEM();
+        ModLootModifiers.register(bus);
         MinecraftForge.EVENT_BUS.register(this);
     }
 
