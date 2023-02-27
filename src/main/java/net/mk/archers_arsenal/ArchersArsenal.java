00c0d8b18e9d9c55b6c4e2897bcc4e6e78760626
Midknight-Maxielle <93679941+Midknight-Maxielle@users.noreply.github.com>
2023-02-27 16:45:11 -0500

    Correcting to .2 gradle

---
+++ src/main/java/net/mk/archers_arsenal/ArchersArsenal.java
@@ -0,0 +1,16 @@
+package net.mk.archers_arsenal;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ArchersArsenal implements ModInitializer {
+
+	public static final String MOD_ID = "archers_arsenal";
+	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
